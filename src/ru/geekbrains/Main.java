96679a036b51192ec28d3fea31423223511486c2
Andrey <MrAbhorrent@yandex.ru>
2020-06-21 01:17:43 +0500

    1. Исправление орфографических ошибок. 2. Изменение предупреждения Warning:(28, 13) Local variable 'result' is redundant 3. Переписана процедура LeapYear

--- src/ru/geekbrains/Main.java
+++ src/ru/geekbrains/Main.java
@@ -19,14 +19,13 @@ public class Main {
         // Boolean types
         boolean isFlag = false;
 
-        
+
     }
 
     // 3. Написать метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат,где a, b, c, d – входные параметры этого метода;
     public static int SomeMathFunc(int a, int b, int c, int d) {
 
-        int result = a * (b + (c/d));
-        return result;
+        return a * (b + (c/d));
     }
 
     // 4. Написать метод, принимающий на вход два числа, и проверяющий что их сумма лежит в пределах от 10 до 20(включительно), если да – вернуть true, в противном случае – false;
@@ -38,7 +37,7 @@ public class Main {
     // 5. Написать метод, которому в качестве параметра передается целое число, метод должен напечатать в консоль положительное ли число передали, или отрицательное; Замечание: ноль считаем положительным числом.
     public static void printSign(int a) {
         if (a >= 0) {
-            System.out.println("Число \t" + a + "\tполодительное");
+            System.out.println("Число \t" + a + "\tположительное");
         } else {
             System.out.println("Число \t" + a + "\tотрицательное");
         }
@@ -58,13 +57,19 @@ public class Main {
     // 8. * Написать метод, который определяет является ли год високосным, и выводит сообщение в консоль. Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый 400-й – високосный.
     public static void LeapYear(int year) {
 
-        if ((year % 4 == 0) || (year % 100 != 0)) {
-            System.out.println("Год " + year + " = високосный");
-        } else if ((year % 400 == 0) || (year % 100 != 0)) {
-            System.out.println("Год " + year + " = високосный");
+        String startMsg = "Год " + year;
+        //boolean specialcase = (year % 100 == 0);
+
+        if (year % 400 == 0) {
+            System.out.println(startMsg + " = високосный");
+        } else if (year % 100 == 0 ) {
+            System.out.println(startMsg + " = не високосный");
+        } else if (year % 4 == 0) {
+            System.out.println(startMsg + " = високосный");
         } else {
-            System.out.println("Год " + year + " = не високосный");
+            System.out.println(startMsg + " = не високосный");
         }
+
     }
 
 }
